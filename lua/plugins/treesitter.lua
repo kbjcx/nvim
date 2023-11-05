2407b6064731417abb1b9192fa0b0e39d65baaeb
kbjcx <2827773209@qq.com>
2023-11-05 14:59:12 +0800

    first

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "vim", "bash", "c", "cpp", "javascript", "json", "lua", "python" },
+    highlight = { enable = true },
+    indent = { enable = true }, 
+
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    }
+} 
+
