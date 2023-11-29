f7142f506f80fb40ce25b01eb54921feec464c57
kbjcx <leitch.lu@outlook.com>
2023-11-29 22:45:29 +0800

    clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,3 +20,25 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
 }
+
+require("lspconfig").clangd.setup {
+    cmd = {
+        "clangd",
+        "--log=verbose",
+        "--pretty",
+        "--all-scopes-completion",
+        "--completion-style=bundled",
+        "--cross-file-rename",
+        "--header-insertion-decorators",
+        "--background-index",
+        "--clang-tidy",
+        "--clang-tidy-checks=cppcoreguidelines-*,performance-*,bugprone-*,portability-*,modernize-*,google-*",
+        "--fallback-style=Google",
+        "-j=12",
+        "--pch-storage=memory",
+        "--function-arg-placeholders=false",
+        "--compile-commands-dir=${workspaceFolder}/build",
+        "--completion-parse=auto"
+    },
+    filetypes = {"c", "cpp", "objc", "objcpp", "cuda"}
+}
