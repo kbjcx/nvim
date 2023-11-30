fa58f81faec65bc24b74399f467b1fc6e90561ca
kbjcx <leitch.lu@outlook.com>
2023-11-30 10:57:10 +0800

    clang-format stylua pya

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,55 @@
+local util = require("formatter.util")
+
+require("formatter").setup({
+	filetype = {
+		-- Formatter configurations for filetype "lua" go here
+		-- and will be executed in order
+		lua = {
+			-- "formatter.filetypes.lua" defines default configurations for the
+			-- "lua" filetype
+			require("formatter.filetypes.lua").stylua,
+
+			-- You can also define your own configuration
+			function()
+				-- Supports conditional formatting
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
+
+				-- Full specification of configurations is down below and in Vim help
+				-- files
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+		cpp = {
+			require("formatter.filetypes.cpp").clangformat,
+			function()
+				return {
+					exe = "clang-format",
+					args = {},
+					stdin = true,
+				}
+			end,
+		},
+	},
+})
+
+-- format on save
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,49 @@
 require("mason").setup({
-  ui = {
-      icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-      }
-  }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "lua_ls",
-  },
+	-- 确保安装，根据需要填写
+	ensure_installed = {
+		"lua_ls",
+		"clangd",
+	},
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
-}
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+})
 
-require("lspconfig").clangd.setup {
-    cmd = {
-        "clangd",
-        "--log=verbose",
-        "--pretty",
-        "--all-scopes-completion",
-        "--completion-style=bundled",
-        "--cross-file-rename",
-        "--header-insertion-decorators",
-        "--background-index",
-        "--clang-tidy",
-        "--clang-tidy-checks=cppcoreguidelines-*,performance-*,bugprone-*,portability-*,modernize-*,google-*",
-        "--fallback-style=Google",
-        "-j=12",
-        "--pch-storage=memory",
-        "--function-arg-placeholders=false",
-        "--compile-commands-dir=${workspaceFolder}/build",
-        "--completion-parse=auto"
-    },
-    filetypes = {"c", "cpp", "objc", "objcpp", "cuda"}
-}
+require("lspconfig").clangd.setup({
+	cmd = {
+		"clangd",
+		"--log=verbose",
+		"--pretty",
+		"--all-scopes-completion",
+		"--completion-style=bundled",
+		"--cross-file-rename",
+		-- "--header-insertion-decorators",
+		"--background-index",
+		"--clang-tidy",
+		"--clang-tidy-checks=cppcoreguidelines-*,performance-*,bugprone-*,portability-*,modernize-*,google-*",
+		"--fallback-style=Google",
+		"-j=12",
+		"--pch-storage=memory",
+		"--function-arg-placeholders=true",
+		"--compile-commands-dir=${workspaceFolder}/build",
+		"--completion-parse=auto",
+	},
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+})
+
+require("lspconfig").cmake.setup({})
+
+require("lspconfig").pylsp.setup({})
